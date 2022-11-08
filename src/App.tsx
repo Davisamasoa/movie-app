8db09394fd7c37e2d88f798cd7be5db92dad4f38
Davi Samuel <davisamasoa@gmail.com>
2022-11-08 19:22:22 -0300

    feat(service): api setup and categories created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+//{import.meta.env.VITE_API_KEY}
+
+import { useEffect, useState } from "react";
+
 export function App() {
-	return <h1 className="text-9xl text-amber-400">Hello vite</h1>;
+	return <h1 className="text-9xl text-amber-400"></h1>;
 }
