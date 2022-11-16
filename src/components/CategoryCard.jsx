80c614dc08a3c2666bc0f60203e512804657f306
Davi Samuel <davisamasoa@gmail.com>
2022-11-16 16:15:44 -0300

    feat: navigation buttons add

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef, useState } from "react";
+
+export function CategoryCard({ title, imageUrl, movie }) {
+	const movieWidth = useRef();
+	useEffect(() => {
+		movie(movieWidth.current.offsetWidth);
+	}, []);
+
+	window.addEventListener("resize", () => {
+		movie(movieWidth.current.offsetWidth);
+	});
+
+	return (
+		<picture ref={movieWidth} className="cursor-pointer relative">
+			<img
+				className="transition-all z-0 duration-300  md:w-40 lg:w-52 sm:w-24 w-24  hover:scale-125"
+				src={`https://image.tmdb.org/t/p/w500/${imageUrl}`}
+			/>
+		</picture>
+	);
+}
